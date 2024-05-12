7c0ffc661c80eba160d11a6d29ed374232d10e19
Benjamin Weinhardt <bjew@gmx.net>
2024-05-12 21:05:00 +0200

    init

---
+++ client/src/main.py
@@ -0,0 +1,45 @@
+import socket
+import pygame, sys
+from settings import *
+from level import Level
+
+class Game:
+	def __init__(self):
+		# # TODO connect to server
+		# server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+		# try:
+		# 	server.connect((SERVER_IP, SERVER_PORT))
+		# except:
+		# 	print(f"ERROR: Server {SERVER_IP}:{SERVER_PORT} not found!")
+		# 	sys.exit(1)
+		# general setup
+		pygame.init()
+		self.screen = pygame.display.set_mode((WIDTH,HEIGTH))
+		pygame.display.set_caption('MMORPG')
+		self.clock = pygame.time.Clock()
+
+		self.level = Level()
+
+		# sound 
+		main_sound = pygame.mixer.Sound('client/audio/main.ogg')
+		main_sound.set_volume(0.2)
+		main_sound.play(loops = -1)
+	
+	def run(self):
+		while True:
+			for event in pygame.event.get():
+				if event.type == pygame.QUIT:
+					pygame.quit()
+					sys.exit()
+				if event.type == pygame.KEYDOWN:
+					if event.key == pygame.K_m:
+						self.level.toggle_menu()
+
+			self.screen.fill(WATER_COLOR)
+			self.level.run()
+			pygame.display.update()
+			self.clock.tick(FPS)
+
+if __name__ == '__main__':
+	game = Game()
+	game.run()
